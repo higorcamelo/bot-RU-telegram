5c94d974f3533dfe6fd1becc0f0478915645f9aa
Higor Camelo <higorcamelo@gmail.com>
2023-08-24 20:18:36 -0300

    Create scraping.py
    Tava com preguiça de fazer mais

---
+++ scraping.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from selenium import webdriver
+from bs4 import beautifulsoup
+
+url = 'https://www.ufc.br/restaurante/cardapio/5-restaurante-universitario-de-quixada' #+/data-de-hoje
