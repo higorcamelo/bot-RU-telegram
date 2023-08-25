ccd59051982a519e856d0b785853b68e9fc355d4
Higor Camelo <higorcamelo@gmail.com>
2023-08-25 20:33:06 -0300

    Geckodriver
    Faltei ter um filho corrigindo o  Selenium, cansei

--- scraping.py
+++ scraping.py
@@ -5,18 +5,18 @@ from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 
 url = 'https://www.ufc.br/restaurante/cardapio/5-restaurante-universitario-de-quixada/'
+driver_path = '/caminho/para/geckodriver'
 
 def acesso_site():
     data_temp = datetime.now()
     data_hoje = data_temp.strftime('%Y-%m-%d')
     option = Options()
     option.headless = True
-    driver = webdriver.Firefox(options=option)
+    driver = webdriver.Firefox(executable_path="geckodriver")
     driver.get(url + data_hoje)
     
     #soup = BeautifulSoup(driver.page_source, 'html.parser')
         
     #driver.quit()
-    print(data_hoje)
 
 acesso_site()
