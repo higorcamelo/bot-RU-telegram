f12fc867e1feb20aad1fa99b06abe3c5ddcd9dbd
Higor Camelo <higorcamelo@gmail.com>
2023-08-26 12:59:31 -0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -14,7 +14,15 @@ def acesso_site():
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     tabela_almoco = soup.find('table', class_='refeicao almoco')
     tabela_jantar = soup.find('table', class_='refeicao jantar')
+    print('---Almoço---')
     for linhas in tabela_almoco.find_all('tr'):
+        celulas= linhas.find_all('td')
+        if celulas:
+            for celula in celulas:
+                print(celula.text)
+                
+    print('---Jantar---')
+    for linhas in tabela_jantar.find_all('tr'):
         celulas= linhas.find_all('td')
         if celulas:
             for celula in celulas:
