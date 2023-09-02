a5f0799160ba88c36b0a1790c8ad861fc563bd7e
Higor Camelo <higorcamelo@gmail.com>
2023-09-02 18:04:23 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import time
 import schedule
-import telegram.ext
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import scraping.py as scp
 
 TOKEN = open('token.txt', 'r').read()
