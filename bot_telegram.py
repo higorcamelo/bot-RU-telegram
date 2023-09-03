d278e4de2935e588c03600b3b145d273600b29da
Higor Camelo <higorcamelo@gmail.com>
2023-09-03 19:59:35 -0300

    Refatoração do scraping e desenvolvimento do bot

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,8 +1,43 @@
 from datetime import datetime
 import time
+import logging
 import schedule
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import scraping.py as scp
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+from scraping import montar_mensagem, setup_scraping
+from config import token_telegram
 
-TOKEN = open('token.txt', 'r').read()
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+logger = logging.getLogger(__name__)
+
+# Função para lidar com o comando /start
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Opa! Sou o JaBOT Al Mossar.")
+    
+def enviar_cardapio(update, context):
+    context.bot.send_message(chat_id = update.effective_chat, text = montar_mensagem(True))
+    
+start_handler = CommandHandler('start', start)
+enviar_cardapio_handler = CommandHandler('cardapioStart', enviar_cardapio)
+
+def main() -> None:
+    updater = Updater(token=token, use_context=True)
+
+    dp = updater.dispatcher
+
+    # Registrar os manipuladores de comandos e mensagens
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler('forceCardapio', enviar_cardapio))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    # Iniciar o bot
+    updater.start_polling()
+
+    # Mantenha o bot em execução até que seja interrompido manualmente (Ctrl + C)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
--- scraping.py
+++ scraping.py
@@ -1,17 +1,29 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.firefox.options import Options
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
-import re
 
-url = 'https://www.ufc.br/restaurante/cardapio/5-restaurante-universitario-de-quixada/'
 
 def acesso_site():
+    # Obtenha a data atual
+    data_atual = datetime.now()
+
+    # Verifique se a data atual é um sábado (5) ou domingo (6)
+    # Se for, subtrai um dia para obter a última sexta-feira (4)
+    if data_atual.weekday() == 5:  # Sábado
+        data_atual -= timedelta(days=1)
+    elif data_atual.weekday() == 6:  # Domingo
+        data_atual -= timedelta(days=2)
+
+    # Formate a data no formato 'YYYY-MM-DD'
+    data_formatada = data_atual.strftime('%Y-%m-%d')
+    url = 'https://www.ufc.br/restaurante/cardapio/5-restaurante-universitario-de-quixada/' + data_formatada
+    
     option = Options()
     option.add_argument('-headless') 
     driver = webdriver.Firefox() #Adicionar options depois
-    driver.get(url+ '2023-08-28') #Data teste
+    driver.get(url)
     
     # Aguarde o carregamento dinâmico (ajuste o tempo conforme necessário)
     driver.implicitly_wait(10)  # Espera até 10 segundos
@@ -40,25 +52,54 @@ def para_json(tabela):
         cols = linha.find_all('td')
         if cols:
             categoria = cols[0].get_text(strip=True)
-            itens = [desc.get_text(strip=True) for col in cols[1:] for desc in col.find_all('span', class_='desc')]
+            itens = []
+            for col in cols[1:]:
+                descs = col.find_all('span', class_='desc')
+                for desc in descs:
+                    itens.append(desc.get_text(strip=True))
             categorias[categoria].extend(itens)
     
-    return categorias
+    return formatar_json(categorias)
 
-def montar_mensagem(almoco):
-    if almoco == True:
+def formatar_json(json_original):
+    json_formatado = {}
+
+    for categoria, itens in json_original.items():
+        json_formatado[categoria] = []
+        descricao = ""  # Inicialize a descrição como uma string vazia
+
+        for item in itens:
+            if '(' in item:
+                # Se o item contém uma descrição entre parênteses, armazene-o na variável 'descricao'
+                descricao = item
+            else:
+                # Se não contém descrição, adicione o item atual e a descrição (se houver) ao JSON formatado
+                json_formatado[categoria].append(item + " " + descricao)
+
+    return json_formatado #TODO: Reprogramar essa função para concatenar as observações de alérgenos 
+
+def montar_mensagem(refeicao='almoco'):
+    if refeicao == 'almoco':
         mensagem_cardapio = 'Bom dia, alunos'
     else:
         mensagem_cardapio = 'Boa tarde, alunos'
         
     return mensagem_cardapio
 
-tabela_almoco, tabela_jantar = acesso_site()
-almoco_json = para_json(tabela_almoco)
-jantar_json = para_json(tabela_jantar)
+def setup_scraping(refeicao='almoco'):
+    if refeicao not in ['almoco', 'jantar']:
+        raise ValueError("Refeição deve ser 'almoco' ou 'jantar'")
 
-with open('almoco.json', 'w', encoding='utf-8') as arquivo_almoco:
-    json.dump(almoco_json, arquivo_almoco, indent=4, ensure_ascii=False)
+    tabela_almoco, tabela_jantar = acesso_site()
 
-with open('jantar.json', 'w', encoding='utf-8') as arquivo_jantar:
-    json.dump(jantar_json, arquivo_jantar, indent=4, ensure_ascii=False)
+    if refeicao == 'almoco':
+        cardapio = para_json(tabela_almoco)
+        arquivo_nome = 'almoco.json'
+    else:
+        cardapio = para_json(tabela_jantar)
+        arquivo_nome = 'jantar.json'
+
+    with open(arquivo_nome, 'w', encoding='utf-8') as arquivo:
+        json.dump(cardapio, arquivo, indent=4, ensure_ascii=False)
+
+
