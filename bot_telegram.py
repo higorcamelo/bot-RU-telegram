332d645367647e21922953877358ef1905d13b9b
Higor Camelo <higorcamelo@gmail.com>
2023-09-05 13:30:27 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,43 +1,32 @@
 from datetime import datetime
-import time
 import logging
-import schedule
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
-from scraping import montar_mensagem, setup_scraping
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from scraping import montar_mensagem
 from config import token_telegram
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-
 logger = logging.getLogger(__name__)
 
 # Função para lidar com o comando /start
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Opa! Sou o JaBOT Al Mossar.")
-    
-def enviar_cardapio(update, context):
-    context.bot.send_message(chat_id = update.effective_chat, text = montar_mensagem(True))
-    
-start_handler = CommandHandler('start', start)
-enviar_cardapio_handler = CommandHandler('cardapioStart', enviar_cardapio)
+async def start(update: Update, context: CallbackContext) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Opa! Sou o JaBOT Al Mossar.")
+
+# Função para lidar com o comando /cardapioStart
+async def enviar_cardapio(update: Update, context: CallbackContext) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=montar_mensagem(True))
 
 def main() -> None:
-    updater = Updater(token=token, use_context=True)
+    application = Application.builder().token(token_telegram).build()
 
-    dp = updater.dispatcher
+    # Registrar os manipuladores de comandos
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler('cardapioStart', enviar_cardapio))
 
-    # Registrar os manipuladores de comandos e mensagens
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler('forceCardapio', enviar_cardapio))
-    dp.add_handler(MessageHandler(Filters.command, unknown))
-
-    # Iniciar o bot
-    updater.start_polling()
-
-    # Mantenha o bot em execução até que seja interrompido manualmente (Ctrl + C)
-    updater.idle()
+    # Executar o bot
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
