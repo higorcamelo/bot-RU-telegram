98591c1e3c36c4df8c23a4789d6f11402e651b40
Higor Camelo <higorcamelo@gmail.com>
2023-09-14 22:09:29 -0300

    Update bot_telegram.py
    MEU DEUS QUE BOT CHATO DE PROGRAMAR

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,71 +6,84 @@ from telegram.ext import Application, CommandHandler, CallbackContext, JobQueue
 from scraping import montar_mensagem, setup_scraping
 from config import token_telegram
 
+# Initialize logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
+# Global variable for menu switch
+switchMenu = False
 
-switchCardapio = False  # Defina a variável globalmente
-
-def executar_scraping():
+# Function to execute scraping
+def execute_scraping():
     setup_scraping('almoco')
     setup_scraping('jantar')
 
-# Função para lidar com o comando /start
-async def startCardapio(update: Update, context: CallbackContext) -> None:
-    global switchCardapio  # Declare a variável global
+# Command handler to start menu
+async def startMenu(update: Update, context: CallbackContext) -> None:
+    global switchMenu
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Opa! Sou o JaBOT Al Mossar. Agora você receberá o cardápio para almoço e jantar!")
-    switchCardapio = True
-    
-async def stopCardapio(update:Update, context: CallbackContext) -> None:
-    global switchCardapio  # Declare a variável global
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Tudo bem, agora você deixará de receber o cardápio...")
-    switchCardapio = False
+    switchMenu = True
 
-# Função para lidar com o comando /cardapioStart
-async def printAlmoco(update: Update, context: CallbackContext) -> None:
+# Command handler to stop menu
+async def stopMenu(update: Update, context: CallbackContext) -> None:
+    global switchMenu
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Tudo bem, agora você deixará de receber o cardápio...")
+    switchMenu = False
+
+# Command handler to print lunch menu
+async def printLunch(update: Update, context: CallbackContext) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id, text=montar_mensagem('almoco'))
-    
-async def printJantar(update: Update, context: CallbackContext) -> None:
+
+# Command handler to print dinner menu
+async def printDinner(update: Update, context: CallbackContext) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id, text=montar_mensagem('jantar'))
 
-# Função para enviar mensagens de almoço com base no agendamento
-def enviar_mensagem_almoco(context: CallbackContext):
-    if switchCardapio:
-        context.bot.send_message(chat_id=context.job.context, text=montar_mensagem('almoco'))
+# Function to send menu based on conditional scheduling
+def sendMenu(context: CallbackContext):
+    if switchMenu:
+        context.bot.send_message(chat_id=context.job.context, text=montar_mensagem(context.job.name))
 
 def main() -> None:
     application = Application.builder().token(token_telegram).build()
 
-    # Registrar os manipuladores de comandos
-    application.add_handler(CommandHandler("qxdStart", startCardapio))
-    application.add_handler(CommandHandler("qxdStop", stopCardapio))
-    application.add_handler(CommandHandler('cardapioAlmoco', printAlmoco))
-    application.add_handler(CommandHandler('cardapioJantar', printJantar))
-    
-    # Obter o objeto JobQueue
-    job_queue: JobQueue = application.job_queue
+    # Register command handlers
+    application.add_handler(CommandHandler("startMenu", startMenu))
+    application.add_handler(CommandHandler("stopMenu", stopMenu))
+    application.add_handler(CommandHandler('printLunch', printLunch))
+    application.add_handler(CommandHandler('printDinner', printDinner))
 
-    # Agendar a função executar_scraping de segunda a sexta-feira às 6h da manhã
-    job_queue.run_daily(
-        executar_scraping,
-        days=(0, 1, 2, 3, 4),  # Segunda a sexta
-        time=datetime.time(hour=23, minute=22, second=0),  # 6:00 AM
+    # Get the JobQueue instance for scraping
+    scraping_job_queue: JobQueue = application.job_queue
+
+    # Schedule the execute_scraping function from Monday to Friday at 6 AM
+    scraping_job_queue.run_daily(
+        execute_scraping,
+        days=(0, 1, 2, 3, 4),  # Monday to Friday
+        time=datetime.time(hour=19, minute=33, second=0),  # 6:00 AM
     )
 
-    # Agendar o envio de mensagens de almoço com base no agendamento condicional
-    #job_queue.run_daily(
-    #    enviar_mensagem_almoco,
-    #    days=(0, 1, 2, 3, 4),  # Segunda a sexta
-    #    time=datetime.time(hour=23, minute=14, second=0),  # 10:30 AM
-    #    context=application.bot.get_updates().effective_chat.id  # Obtém o chat_id
-    #)
+    # Get separate JobQueue instances for lunch and dinner
+    lunch_job_queue: JobQueue = application.job_queue
+    dinner_job_queue: JobQueue = application.job_queue
 
-    # Executar o bot
+    # Schedule lunch menu sending at a specific time
+    lunch_job_queue.run_daily(
+        sendMenu,
+        days=(0, 1, 2, 3, 4),  # Monday to Friday
+        time=datetime.time(hour=12, minute=0, second=0),  # 12:00 PM
+        data='almoco',
+    )
+
+    # Schedule dinner menu sending at a specific time
+    dinner_job_queue.run_daily(
+        sendMenu,
+        days=(0, 1, 2, 3, 4),  # Monday to Friday
+        time=datetime.time(hour=18, minute=0, second=0),  # 6:00 PM
+        data='jantar',
+    )
+
+    # Run the bot
     application.run_polling(allowed_updates=Update.ALL_TYPES)
     
-    #TODO: DESCOBRIR COMO PROGRAMAR O CRONOGRAMA DO SCRAPING E DAS MENSAGENS
-
 if __name__ == '__main__':
     main()
