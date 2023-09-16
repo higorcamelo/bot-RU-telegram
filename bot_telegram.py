0fa1aa02212d658f4788930545a30c27b7787627
Higor Camelo <higorcamelo@gmail.com>
2023-09-16 11:26:55 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,6 +1,7 @@
 import logging
 import asyncio
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 from scraping import montar_mensagem, setup_scraping
@@ -8,12 +9,12 @@ from config import token_telegram
 
 # Initialize logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO, filename='logging_bot.log')
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger('httpx').setLevel(logging.WARNING)
 
 # Global variable for menu switch
 switchMenu = False
 
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(timezone="America/Sao_Paulo")
 
 # Function to execute scraping
 def execute_scraping():
@@ -29,15 +30,16 @@ async def startMenu(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id=chat_id, text="Opa! Sou o JaBOT Al Mossar. Agora você receberá o cardápio para almoço e jantar!")
     switchMenu = True
+    logging.info(switchMenu)
     
-    scheduler.add_job(sendMenuWrapper, 'cron', args=['almoco', chat_id, context], day_of_week='mon-fri', hour=19, minute=9)
-    scheduler.add_job(sendMenuWrapper, 'cron', args=['jantar', chat_id, context], day_of_week='mon-fri', hour=19, minute=10) #TODO: Erro aqui? Só uma mensagem é enviada
+    # Schedule the menu sending jobs
+    scheduler.add_job(sendMenuAsync, CronTrigger(day_of_week='mon-sat', hour=11, minute=12), args=['almoco', chat_id, context])
+    scheduler.add_job(sendMenuAsync, CronTrigger(day_of_week='mon-sat', hour=11, minute=13), args=['jantar', chat_id, context])
 
 # Command handler to stop menu
 async def stopMenu(update: Update, context: CallbackContext) -> None:
     global switchMenu
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Tudo bem, agora você deixará de receber o cardápio...")
-    switchMenu = False
 
 # Command handler to print lunch menu
 async def printLunch(update: Update, context: CallbackContext) -> None:
@@ -49,27 +51,24 @@ async def printDinner(update: Update, context: CallbackContext) -> None:
 
 # Define an asynchronous function that sends the menu
 async def sendMenuAsync(tipo_refeicao: str, chat_id, context: CallbackContext):
-    if switchMenu:
-        montagem = montar_mensagem(tipo_refeicao)
-        await context.bot.send_message(chat_id=chat_id, text=montagem)
+    try:
+        if switchMenu:
+            logging.info(f"Chamando sendMenuAsync para {tipo_refeicao}")
+            montagem = montar_mensagem(tipo_refeicao)
+            await context.bot.send_message(chat_id=chat_id, text=montagem) #TODO NAO SEI MAIS O QUE FAZER
+    except Exception as e:
+        logging.error(f"Error in sendMenuAsync: {str(e)}")
 
-# Define a synchronous wrapper function that runs the asynchronous function
-def sendMenuWrapper(tipo_refeicao: str, chat_id, context: CallbackContext):
-    if switchMenu:
-        asyncio.run(sendMenuAsync(tipo_refeicao, chat_id, context))
-    
 def main() -> None:
     application = Application.builder().token(token_telegram).build()
-
+    
     # Register command handlers
     application.add_handler(CommandHandler("start_cardapio", startMenu))
     application.add_handler(CommandHandler("stop_cardapio", stopMenu))
     application.add_handler(CommandHandler('cardapio_almoco', printLunch))
     application.add_handler(CommandHandler('cardapio_jantar', printDinner))
-     
-    chat_id = None
     
-    scheduler.add_job(schedule_scraping, 'cron', day_of_week='mon-fri', hour=6, minute=00)
+    scheduler.add_job(schedule_scraping, CronTrigger(day_of_week='mon-fri', hour=6, minute=0))
     
     # Start the scheduler
     scheduler.start()
