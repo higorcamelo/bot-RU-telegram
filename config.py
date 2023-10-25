c81a8aa13ed5483389d312ca76b92a5cac067724
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2023-10-25 16:40:06 -0300

    config.py de exemplo

---
+++ config.py
@@ -0,0 +1,8 @@
+# Arquivo de exemplo, preencher os campos corretamente
+
+token_telegram = '<MEU ID DO TELEGRAM>'
+urlRU = 'MINHA URL'
+logging_level = 20
+logging_file = None
+scraping_hour = 9
+id_feedback_chat = -1234567890
