956b118a61f521065f03a4895a4222311853f49c
Higor Camelo <higorcamelo@gmail.com>
2023-10-25 16:39:15 -0300

    Mudança no config,py

---
+++ config.py
@@ -0,0 +1,7 @@
+token_telegram = '123456789:AAGci8-HFUIHIOUFhnsohasdhsakddjhasdhiods' #PROD
+token_telegram_dev = '987654321:dsfjlkLKFJDSKLNfdjqakljdslkadddd' #DEV
+urlRU = 'https://www.ufc.br/restaurante/cardapio/5-restaurante-universitario-de-quixada/'
+scraping_hour = 6
+id_feedback_chat = -456987123321
+logging_level = 20
+logging_file = 'bot.log'
